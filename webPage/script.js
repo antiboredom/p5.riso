b51a10e2e243d2e71e3d3d8a27d5d754d55fc680
Paolla <pbd244@nyu.edu>
2019-08-06 10:34:16 -0400

    web

---
+++ webPage/script.js
@@ -0,0 +1,24 @@
+function show_ins() {
+    var x = document.getElementById("openPro");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function show_ins2() {
+    var x = document.getElementById("localIns");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
+function show_ins3() {
+    var x = document.getElementById("p5Ins");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
