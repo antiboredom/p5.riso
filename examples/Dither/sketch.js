4ff3ec3234da8d7ebbafa0ae2f41b3f8adc34547
Sam <splavigne@gmail.com>
2019-08-08 21:36:58 -0400

    add dither example

---
+++ examples/Dither/sketch.js
@@ -0,0 +1,39 @@
+let black;
+let img;
+let ditherType = 'atkinson';
+
+function preload() {
+  img = loadImage('data/no_threat.jpg');
+}
+
+function setup() {
+  pixelDensity(1);
+  createCanvas(img.width, img.height);
+
+  black = new Riso('black');
+}
+
+function draw() {
+  background(220);
+
+  let threshold = map(mouseX, 0, width, 0, 255);
+
+  clearRiso();
+
+  let dithered = ditherImage(img, ditherType, threshold);
+  black.image(dithered, 0, 0);
+
+  drawRiso();
+}
+
+function mouseClicked() {
+  exportRiso();
+}
+
+function keyReleased() {
+  if (key == 1) ditherType = 'atkinson';
+  else if (key == 2) ditherType = 'floydsteinberg';
+  else if (key == 3) ditherType = 'bayer';
+  else if (key == 4) ditherType = 'none';
+}
+
