738b50c6691e4064c2a9c3579167b196d20ec4d9
Tega Brain <tega.brain@gmail.com>
2019-08-14 00:56:44 -0400

    dithering documented

---
+++ examples/Color_test/readme.md
@@ -0,0 +1,8 @@
+# Color test print
+
+Test print for layering three colors, two color or one color at different opacities
+[Run it with the p5 editor]()
+
+```javascript
+coming soon.
+```
---
+++ examples/Color_test/sketch.js
@@ -0,0 +1,8 @@
+
+//  * RISOGRAPH:
+//  * This Sketch was made to display different color combinations
+//  * [SWATCHES:] displays a single color at different opacities
+//  * [DUORISO:] displays how two different colors with different opacities can mix to form new colors
+//  * [TRIORISO:] displays how three different colors with different opacities can mix to form new colors
+//  * Test prints done with RED, BLUE, YELLOW, and BLACK
+
